e6ccec1db305d616cf01e37f4a9addf2af4cf0fc
Ayaz Ayupov <ayaz@releaseband.com>
2021-04-12 17:27:43 +0300

    GRP-594 remove all codes

--- metrics.go
+++
@@ -1,7 +0,0 @@
-package metrics
-
-import "context"
-
-type Metrics interface {
-	MeasureLatency(ctx context.Context, entity, method string, callback func())
-}
---
+++ opencensus/views/views.go
@@ -0,0 +1,32 @@
+package opencensus
+
+import (
+	"go.opencensus.io/stats"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
+)
+
+func MakeLatencyView(name, description string, measure stats.Measure, tagKeys []tag.Key) *view.View {
+	return &view.View{
+		Name:        name,
+		Description: description,
+		TagKeys:     tagKeys,
+		Measure:     measure,
+		Aggregation: view.Distribution(0.2, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
+			2048, 4096, 8192, 16384, 32768),
+	}
+}
+
+func MakeCounterView(name, description string, measure stats.Measure, tagKeys []tag.Key) *view.View {
+	return &view.View{
+		Name:        name,
+		Description: description,
+		Measure:     measure,
+		Aggregation: view.Count(),
+		TagKeys:     tagKeys,
+	}
+}
+
+func RegisterViews(views ...*view.View) error {
+	return view.Register(views...)
+}
