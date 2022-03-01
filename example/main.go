57635a824a001ad327b324cb1982d7e8b819111e
Ayaz Ayupov <ayaz@releaseband.com>
2022-03-01 16:16:12 +0300

    GRP-1156 #time 20m changed example

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/releaseband/metrics"
 	"log"
 	"strconv"
 	"time"
@@ -10,9 +11,6 @@ import (
 	"contrib.go.opencensus.io/exporter/prometheus"
 	"github.com/releaseband/metrics/exporter"
 
-	"github.com/releaseband/metrics/opencensus/views"
-
-	"github.com/releaseband/metrics/measure"
 	"go.opencensus.io/tag"
 )
 
@@ -22,52 +20,116 @@ const (
 )
 
 var (
-	counter       *measure.CounterMeasure
-	latency       *measure.LatencyMeasure
-	entityKey     = tag.MustNewKey("entity")
-	workerNameKey = tag.MustNewKey("name")
+	command = tag.MustNewKey("command")
+	id      = tag.MustNewKey("id")
+	method  = tag.MustNewKey("method")
 )
 
-func worker(i int64) {
-	workerName := strconv.FormatInt(i, 10) + "sec"
+var (
+	latency     = metrics.NewLatency("latency_name", "latency description")
+	httpLatency = metrics.NewLatency("http_latency", "http latency")
+	counter     = metrics.NewCounter("iterations", "iterations")
+)
 
-	fmt.Println("worker " + workerName + " started")
+func a(ctx context.Context, i int) {
+	ctx, record, err := metrics.Start(context.Background(), latency,
+		tag.Insert(command, "a"),
+		tag.Insert(id, strconv.Itoa(i)),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Duration(i) * time.Millisecond)
+
+	record()
+}
+
+func httpSend(ctx context.Context, i int) {
+	ctx, record, err := metrics.Start(ctx, httpLatency,
+		tag.Insert(method, "POST"),
+		tag.Insert(id, strconv.Itoa(i)),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if i%2 == 0 {
+		time.Sleep(200 * time.Millisecond)
+	} else {
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	record()
+}
+
+func c(ctx context.Context, i int) {
+	ctx, record, err := metrics.Start(ctx, latency,
+		tag.Insert(command, "c"),
+		tag.Insert(id, strconv.Itoa(i)),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Duration(i) * time.Millisecond)
+
+	httpSend(ctx, i)
+
+	record()
+}
+
+func b(ctx context.Context, i int) {
+	newCtx, record, err := metrics.Start(ctx, latency,
+		tag.Insert(command, "b"),
+		tag.Insert(id, strconv.Itoa(i)),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	c(ctx, i) //
+
+	time.Sleep(time.Duration(i) * time.Millisecond)
+
+	record()
+
+	_ = newCtx
+}
+
+func worker(i int) {
+	fmt.Println("worker ", i, " started")
 
 	for {
-		ctx, err := tag.New(context.Background(),
-			tag.Insert(entityKey, "worker"),
-			tag.Insert(workerNameKey, workerName),
-		)
+		ctx := context.Background()
 
-		if err != nil {
-			log.Fatal(err)
+		a(ctx, i)
+
+		b(ctx, i)
+
+		if _, err := metrics.AddCount(ctx, counter, 1, tag.Insert(id, strconv.Itoa(i))); err != nil {
+			panic(err)
 		}
-
-		counter.IncrementCounter(ctx)
-
-		start := measure.Start()
-		time.Sleep(time.Duration(i) * time.Second)
-		latency.Record(ctx, measure.End(start))
 	}
 }
 
 func initViews() error {
-	latencyTagKeys := []tag.Key{entityKey, workerNameKey}
-	counterTagKeys := []tag.Key{entityKey, workerNameKey}
+	latencyView := metrics.NewDefaultLatencyView("latency_view",
+		"latency view description", latency, command, id)
+	httpLatencyView := metrics.NewDeliveryServicesLatencyView("http_latency", "http service latency",
+		httpLatency, method, id)
+	counterView := metrics.NewCounterView("counter_view",
+		"counter view description", counter, id)
 
-	latency = measure.NewLatencyMeasure("latency", "The latency measure in milliseconds")
-	counter = measure.NewCounterMeasure("counter", "The counter measure")
-
-	latencyView := views.MakeLatencyView("latency_view",
-		"latency view description", latency.Measure(), latencyTagKeys)
-	counterView := views.MakeCounterView("counter_view",
-		"counter view description", counter.Measure(), counterTagKeys)
-
-	return views.RegisterViews(latencyView, counterView)
+	return metrics.Register(latencyView, counterView, httpLatencyView)
 }
 
 func startWorkers() {
-	for i := int64(1); i < 10; i++ {
+	for i := 0; i < 64; i++ {
 		go worker(i)
 	}
 }
