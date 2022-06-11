b7f8ded3153e29c3ca143b073d4e73f8418492a7
Ayaz Ayupov <ayaz@releaseband.com>
2022-06-11 16:01:56 +0700

    GRP-1222 added func SinceInSecond

--- metrics.go
+++ metrics.go
@@ -5,3 +5,7 @@ import "time"
 func SinceInMs(start time.Time) int64 {
 	return time.Since(start).Milliseconds()
 }
+
+func SinceInSeconds(start time.Time) float64 {
+	return time.Since(start).Seconds()
+}
